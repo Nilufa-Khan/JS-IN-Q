58881dd5a4c180113901f9ed772f4c388affdcae
Nilufa Khan <khannilufa5@gmail.com>
2023-01-16 12:25:49 +0530

    isNan function

--- practice3/operator.js
+++ practice3/operator.js
@@ -53,6 +53,7 @@ console.log("&& operator "+(ope1 && ope3))  // returns undefined
 // isNan function
 
 var val1 = NaN;
+
 var val2 = 78;
 console.log(isNaN(val1)) // true
 console.log(isNaN(val2)) // false
